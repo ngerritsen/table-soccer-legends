e6c615ebc7ff463ed7042a4ea783376a5f0b4bde
Niels Gerritsen <n.c.gerritsen@gmail.com>
2019-02-12 13:33:46 +0100

    Init firebase

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 const app = new Vue({
   el: '#app',
+  created: {
+    initFirebase()
+  },
   data: {
     message: 'Hello Vue!'
   }
 });
+
+function initFirebase() {
+  firebase.initializeApp({
+    apiKey: "AIzaSyCTyxZyDWVmSaJOLUDFZG5OlzoUW4MEH1o",
+    authDomain: "table-soccer-legends.firebaseapp.com",
+    databaseURL: "https://table-soccer-legends.firebaseio.com",
+    projectId: "table-soccer-legends",
+    storageBucket: "table-soccer-legends.appspot.com",
+    messagingSenderId: "225126533819"
+  });
+}
