5328c2e4fd7da5d793ad0a422b21c355c304d4c9
Niels Gerritsen <n.c.gerritsen@gmail.com>
2019-05-24 19:14:36 +0200

    Fix github icon import

--- src/icons.js
+++ src/icons.js
@@ -3,7 +3,7 @@ import fontawesome from '@fortawesome/fontawesome';
 const icons = [
   require('@fortawesome/fontawesome-free-solid/faCircleNotch'),
   require('@fortawesome/fontawesome-free-solid/faTrophy'),
-  require('@fortawesome/fontawesome-free-brands/faGitHub')
+  require('@fortawesome/fontawesome-free-brands/faGithub')
 ];
 
 fontawesome.library.add(...icons);
